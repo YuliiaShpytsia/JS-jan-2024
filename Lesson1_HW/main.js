d06d691e2f44d37fc8fc799456bdfc236ec09ad0
YuliiaShpytsia <yulia93shpiza@gmail.com>
2024-02-22 18:49:27 +0200

    init commit

---
+++ Lesson1_HW/main.js
@@ -0,0 +1,51 @@
+let a = 'hello';
+console.log(a);
+
+let b = 'owu';
+console.log(b);
+
+let c = 'com';
+console.log(c);
+
+let d = 'ua';
+console.log(d);
+
+let e = 1;
+console.log(e);
+
+e = 10;
+console.log(e);
+
+e = -999;
+console.log(e);
+
+e = 123;
+console.log(e);
+
+e = 3.14;
+console.log(e);
+
+e = 2.7;
+console.log(e)
+
+e = 16;
+console.log(e);
+
+let f = true;
+console.log(f)
+
+let g = false;
+console.log(g);
+
+let firstName = 'Yuliia ';
+let middleName = 'Vadymivna ';
+let lastName = 'Shpytsia'
+let fullName = firstName + middleName + lastName
+console.log(fullName)
+
+let x = 100;
+let y = '100';
+let z = true;
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z)
