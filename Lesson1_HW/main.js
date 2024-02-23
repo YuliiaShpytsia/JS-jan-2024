b763354f7da610a49aa3edff11b2d1b89b17c72d
YuliiaShpytsia <yulia93shpiza@gmail.com>
2024-02-23 14:47:46 +0200

    init commit

--- Lesson1_HW/main.js
+++ Lesson1_HW/main.js
@@ -1,3 +1,5 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
 let a = 'hello';
 console.log(a);
 
@@ -37,11 +39,16 @@ console.log(f)
 let g = false;
 console.log(g);
 
-let firstName = 'Yuliia ';
-let middleName = 'Vadymivna ';
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Yuliia';
+let middleName = 'Vadymivna';
 let lastName = 'Shpytsia'
-let fullName = firstName + middleName + lastName
+let fullName = firstName + ' ' + middleName + ' ' + lastName
 console.log(fullName)
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
 
 let x = 100;
 let y = '100';
@@ -49,3 +56,13 @@ let z = true;
 console.log(typeof x);
 console.log(typeof y);
 console.log(typeof z)
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let name = prompt('Enter your name')
+console.log(name)
+let fatherName = prompt('Enter your fatherName')
+console.log(fatherName)
+let age = prompt('Enter your age')
+console.log(age)
